1f7bde8adf111e0f9d3a68b29b2cbfbe6b34456f
Kamilla Kemilly <kamillake@gmail.com>
2013-03-10 17:37:03 -0300

    Eu arrumando a Classe User

--- branches/main/System Resource Management/src/br/ufal/ic/comp215/srm/model/User.java
+++ branches/main/System Resource Management/src/br/ufal/ic/comp215/srm/model/User.java
@@ -4,15 +4,17 @@ public class User {
 	private int id;
 	private String name;
 	private String email;
+	private String login;
 	private String password;
 
 	public User() {
 	}
 
-	public User(int id, String name, String email, String password) {
+	public User(int id, String name, String email, String login, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
+		this.login = login;
 		this.password = password;
 	}
 
@@ -42,6 +44,14 @@ public class User {
 
 	public String getPassword() {
 		return password;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getLogin() {
+		return login;
 	}
 
 	public void setPassword(String password) {
