ac80bae9fe01b4b30d3e1362c6df17c9372e2a08
thiagoeolima <thiagorms18@gmail.com>
2013-03-19 14:59:00 -0300

    start template

--- main/SystemResourceManagement/src/br/ic/ufal/srm/view/View.java
+++ main/SystemResourceManagement/src/br/ic/ufal/srm/view/View.java
@@ -11,7 +11,7 @@ public abstract class View {
 	protected Model model;
 	protected PrintWriter output;
 
-	final void generate(Model model, HttpServletResponse response)
+	public final void generate(Model model, HttpServletResponse response)
 			throws IOException {
 		this.model = model;
 		this.output = response.getWriter();
@@ -22,12 +22,54 @@ public abstract class View {
 
 	}
 
-	public abstract void head();
+	public void head() {
+		output.println("<!DOCTYPE html>");
+		output.println("<html lang=\"en\">");
+		output.println("<head>");
+		output.println("<meta charset=\"utf-8\">");
+		output.println("  <title>Login</title>");
+		output.println("  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+
+		output.println(" <!-- Loading Bootstrap -->");
+		output.println(" <link href=\"css/bootstrap.css\" rel=\"stylesheet\">");
+
+		output.println(" <!-- Loading Flat UI -->");
+		output.println("  <link href=\"css/flat-ui.css\" rel=\"stylesheet\">");
+		output.println(" <link rel=\"shortcut icon\" href=\"images/favicon.ico\">");
+
+		output.println(" <!-- Loading Reset -->");
+		output.println(" <link href=\"css/reset.css\" rel=\"stylesheet\">");
+
+		output.println(" <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->");
+		output.println("  <!--[if lt IE 9]>");
+		output.println("    <script src=\"js/html5shiv.js\"></script>");
+		output.println("  <![endif]-->");
+		output.println(" </head>");
+		output.println(" <body>");
+	}
 
 	public abstract void topMenu();
 
 	public abstract void content();
 
-	public abstract void footer();
+	public void footer() {
+		output.println(" <!-- Load JS here for greater good =============================-->");
+		output.println("  <script src=\"js/jquery-1.8.2.min.js\"></script>");
+		output.println("  <script src=\"js/jquery-ui-1.10.0.custom.min.js\"></script>");
+		output.println(" <script src=\"js/jquery.dropkick-1.0.0.js\"></script>");
+		output.println(" <script src=\"js/custom_checkbox_and_radio.js\"></script>");
+		output.println(" <script src=\"js/custom_radio.js\"></script>");
+		output.println(" <script src=\"js/jquery.tagsinput.js\"></script>");
+		output.println(" <script src=\"js/bootstrap-tooltip.js\"></script>");
+		output.println(" <script src=\"js/jquery.placeholder.js\"></script>");
+		output.println(" <script src=\"http://vjs.zencdn.net/c/video.js\"></script>");
+		output.println(" <script src=\"js/application.js\"></script>");
+		output.println(" <!--[if lt IE 8]>");
+		output.println("    <script src=\"js/icon-font-ie7.js\"></script>");
+		output.println("    <script src=\"js/icon-font-ie7-24.js\"></script>");
+		output.println("    <![endif]-->");
+		output.println("  </body>");
+		output.println("</html>");
+	}
 
 }
--- main/SystemResourceManagement/src/br/ufal/ic/srm/controller/Login.java
+++ main/SystemResourceManagement/src/br/ufal/ic/srm/controller/Login.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import br.ic.ufal.srm.view.View;
+import br.ic.ufal.srm.view.login.LoginView;
 import br.ufal.ic.srm.model.User;
 
 /**
@@ -29,42 +31,39 @@ public class Login extends Controller {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	
-	public void doGet(HttpServletRequest req, HttpServletResponse res)
+
+	public void doGet(HttpServletRequest req, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		HttpSession session = req.getSession();
-		
+
 		System.out.println(session.getAttribute("user"));
-		
-		
-		RequestDispatcher dispatcher = req
-				.getRequestDispatcher("jsp/login.jsp");
-		dispatcher.forward(req, res);
+
+		new LoginView().generate(null, response);
+	
+
 	}
 
 	/**
-	 * @see HttpSHttpServletRequestervlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpSHttpServletRequestervlet#doPost(HttpServletRequest request,
+	 *      HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
 		User user = new User();
-		
+
 		user.setLogin(request.getParameter("login"));
 		user.setPassword(request.getParameter("password"));
-		
-		
+
 		HttpSession session = request.getSession();
-		
+
 		session.setAttribute("user", user);
-		
-		
+
 		if (user.validate()) {
 			// redirecionamento para perfil
 			// salvar o user em session;
-			
+
 		}
 
 		user.errors();
